53e8ab33a3b4363bde43846c1aa78a1412f1b289
Will <willchesson@gmail.com>
2022-05-24 10:48:29 -0400

    commit tree abe96c93b672edb44206af4d2510ecaa0165b80f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+
+  def no_side_zero?
+    a > 0 && b > 0 && c > 0
+    # [a, b, c].all?(&:positive)
+  end
+
+  def triangle_inequality?
+    a + b > c && b + c > a && a + c > b
+  end
+  
+  def validate_triangle
+      raise TriangleError unless no_side_zero? && triangle_inequality?
+    end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else a != b && b != c 
+      :scalene
+    end
+  end
+  
+    class TriangleError < StandardError
+    def message
+      "Sorry, the triangle is invalid. Try again."
+    end
+  end
 end
+
